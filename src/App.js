5ba43c4b3b824d093ba6ca858b156d65ad342c2d
Daniel Cho <daniel.h.cho@student.unsw.edu.au>
2024-01-13 12:58:22 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Cards from './components/Cards';
+import Food from './components/Food';
+import Category from './components/Category';
+
+function App() {
+  return (
+    <div >
+      <Navbar/>
+      <Hero/>
+      <Cards/>
+      <Food/>
+      <Category />
+    </div>
+  );
+}
+
+export default App;
